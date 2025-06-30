5249e10c538c621fdf0b8e1eacdddacfa0358ed5
farrdeen <fardeenkhan7869@gmail.com>
2025-06-30 12:38:53 +0530

    Fix tsconfig2

--- frontend/src/components/protectedroute.tsx
+++ frontend/src/components/protectedroute.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
+import React, { type ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import type { ReactNode } from "react";
 
 type ProtectedRouteProps = {
   children: ReactNode;
